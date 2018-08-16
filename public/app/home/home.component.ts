67760417dadc1bca1538bca637353c8e8db53842
jairoarivas <jairoalbertorivas@gmail.com>
2018-08-16 16:41:03 -0400

    yes

--- public/app/home/home.component.ts
+++ public/app/home/home.component.ts
@@ -30,39 +30,39 @@ export class HomeComponent {
   // setTranslate(xPos, yPos,el){
   //   el.style.transform = "translate3d(" + xPos + ", " + yPos + "px, 0";
   // }
-  
-  displayNextImage(){
 
-    if(this.num >= 3){
-      this.num = 0;
-    }
-    if(this.num === 0 ){
-      document.getElementById('adoImage1').style.display = 'block';
-      document.getElementById('adoImage2').style.display = 'none';
-      document.getElementById('adoImage3').style.display = 'none';
-    }
-    else if(this.num === 1 ){
-      document.getElementById('adoImage1').style.display = 'none';
-      document.getElementById('adoImage2').style.display = 'block';
-      document.getElementById('adoImage3').style.display = 'none';
-    }
-    else if(this.num === 2 ){
-      document.getElementById('adoImage1').style.display = 'none';
-      document.getElementById('adoImage2').style.display = 'none';
-      document.getElementById('adoImage3').style.display = 'block';
-    }
-    this.num ++;
-  }
+  // displayNextImage(){
+  //
+  //   if(this.num >= 3){
+  //     this.num = 0;
+  //   }
+  //   if(this.num === 0 ){
+  //     document.getElementById('adoImage1').style.display = 'block';
+  //     document.getElementById('adoImage2').style.display = 'none';
+  //     document.getElementById('adoImage3').style.display = 'none';
+  //   }
+  //   else if(this.num === 1 ){
+  //     document.getElementById('adoImage1').style.display = 'none';
+  //     document.getElementById('adoImage2').style.display = 'block';
+  //     document.getElementById('adoImage3').style.display = 'none';
+  //   }
+  //   else if(this.num === 2 ){
+  //     document.getElementById('adoImage1').style.display = 'none';
+  //     document.getElementById('adoImage2').style.display = 'none';
+  //     document.getElementById('adoImage3').style.display = 'block';
+  //   }
+  //   this.num ++;
+  // }
   ngOnInit(){
-    this.num = 0;
-    console.debug(this.num);
+    // this.num = 0;
+    // console.debug(this.num);
     // this.image = document.getElementById("welcomeImage");
     // window.addEventListener("DOMContentLoaded", this.scrollLoop, false);
     //when button is clicked becomes true
     this.wasClicked = false;
 
-    let pic = Observable.timer(0,3000);
-    pic.subscribe(this.displayNextImage);
+    // let pic = Observable.timer(0,3000);
+    // pic.subscribe(this.displayNextImage);
     //g hold all the items with class "restOfSite"
     //will be used to hide all none nav menu items when menu is opened
     this.g = document.getElementsByClassName('restOfSite') as HTMLCollectionOf<HTMLElement>;
